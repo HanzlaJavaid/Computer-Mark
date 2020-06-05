c43dec973992553273c2c67584769aaedd39bff9
Hanzla Javaid <hanzla403@gmail.com>
2020-06-05 21:18:15 +0500

    commit to master

--- backend.py
+++ backend.py
@@ -6,27 +6,22 @@ def convert(axis,mode,opcode,address):
         "Y":"1",
         "D":"0",
         "I":"1",
-        "LDA":"000",
+        "LDA":"0000",
         "ADD":"0001",
+        "HLT":"1111",
     }
     x = '{0:b}'.format(int(address))
     for i in range(0,10-len(x)):
         x = "0"+x
-    print(switcher[axis]+switcher[mode]+switcher[opcode]+x) 
-    return switcher[opcode]
+
+    return(switcher[axis]+switcher[mode]+switcher[opcode]+x) 
 
 def change_content(r,content):
     r['text'] = content.get(1.0, END+"-1c")
     print(content)
 
-class MemoryWord():
-    def __init__(self):
-        self.wordsize = 16
-        self.word = "0000000000000000"
-    def __str__(self):
-        return self.word
-
 class Memory():
+
     def __init__(self):
         self.memorysize = 1024
         self.MEMORY = []
@@ -47,7 +42,60 @@ class Memory():
         temp = raw.split('\n')
         for i in range(0,len(temp)-1):
             x = temp[i].split(' ')
-            opcode = convert(x[0],x[1])
-            print(opcode)
+            code = convert(x[0],x[1],x[2],x[3])
+            real = x[0]+x[1]+x[2]+x[3]
+            self.REALMEMORY[i] = real
+            self.MEMORY[i] = code
         
         self.Print()
+
+class Register():
+    def __init__(self,size,t):
+        self.output=None
+        self.bits = size
+        self.value=None
+        self.outputVal="0000000000000000"
+        if(t == "NUM"):
+            self.value = int(0)
+        if(t == "FULL"):
+            self.value = "0000000000000000"
+class Architecture():
+    def __init__(self,acx,acy,pc,ar,ir,dr,inpr,outr,memory):
+        self.x = acx
+        self.y = acy
+        self.pc = pc
+        self.ir = ir
+        self.ar = ar
+        self.dr = dr
+        self.inpr = inpr
+        self.outr = outr
+        self.memory = memory  
+    
+    def ModifyOutput(self,val,outputsource):
+        temp = val.split(' ')
+        outputsource.outputVal = convert(temp[0],temp[1],temp[2],temp[3])
+    
+    #Operations
+    def RUN_PROGRAM(self):
+        for instrucion in self.memory.REALMEMORY:
+            self.PC_TO_AR()
+            self.INCREMENT_PC()
+            self.Mar_TO_IR(self.ar.val)
+
+    def LOAD(self,axis,mode,address):
+        print("This is LDA")
+
+    #Microoperations
+    def INCREMENT_PC(self):
+        self.pc.value = self.pc.val + 1
+        self.pc.outputVal = '{0:b}'.format(int(self.pc.value))
+
+    def PC_TO_AR(self):
+        self.ar.value = self.pc.value
+        self.ModifyOutput(self.ar.value,self.ar)
+
+    def Mar_TO_IR(self,mar):
+        self.ir.value = self.memory.REALMEMORY[mar]
+        self.ModifyOutput(self.ir.value,self.ir)
+
+
